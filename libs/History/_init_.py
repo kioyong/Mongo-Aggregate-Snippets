ad104024011fa84339c1ce7cc358c1de08e65839
LiangYong <yong.a.liang@accenture.com>
2017-09-19 15:25:04 +0800

    revert commit

--- libs/History/_init_.py
+++
@@ -1,1 +0,0 @@
-from helloWorld import sublime
--- libs/test.py
+++
@@ -1,9 +0,0 @@
-# aa ='hello'
-# print(aa)
-# def aaa() :
-#     print("this is aaa")
-# aaa()
-# def bbb():
-#     print("this is bbb")
-# v=True
-# print(v)
